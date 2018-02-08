dfff8f93cdfd173b82e06724480e52e60cbaae45
Felipe Hernandez <dark-sage-sd@hotmail.com>
2018-02-08 11:19:31 -0300

    More database test

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,25 +1,24 @@
 //document.getElementsByTagName("body")[0].setAttribute("display", "none");
 (function($){
     $(function(){
+        $('#modal-preload').modal();
         const url_firebase = 'https://fir-test-52ab4.firebaseio.com/Hosting/.json';
         $.ajax({
             url:   url_firebase,
             type:  'get',
             dataType: "json",
             beforeSend: function () {
-                $("body").css("display", "none");
-                $(".preloader-wrapper").css("display", "block");
+                $('#modal-preload').modal('open');
             },
             success:  function (response) {
-                $("body").css("display", "block");
-                $(".preloader-wrapper").css("display", "none");
-                console.log(response);
+                $('#modal-preload').modal('close');
                 let dataHost;
                 for(data in response){
                     dataHost = response[data];
                 }
                 $('#mat-logo').html(dataHost.logo);
                 $('#header-title').html(dataHost.title);
+                $('#download-button').attr('href', dataHost.button_start);
             }
         });
 
